106df24a26e5d06ac530f16ac19f3170b885003b
Sarah Ting (macbook) <sarah.j.ting@gmail.com>
2020-04-05 19:37:34 +0900

    improve complementary contrast

--- src/palettes/ComplementaryContrastPalette.js
+++ src/palettes/ComplementaryContrastPalette.js
@@ -8,24 +8,25 @@ class ComplementaryContrastPalette extends Palette {
     generate(seed = null) {
         if(seed === null) seed = Color.generateSeed();
 
-        // randomise gradient
-
-        // get left color
         const left = seed.clone();
         left.v += Math.random() * 50;
         left.s = Math.random() * 100;
-
-        // get right color
         const right = seed.clone();
         right.v = Math.random() * 20;
         right.s = Math.random() * 100;
 
         const complement = Color.generateComplement(seed, 60);
         complement.v += Math.random() * 50;
+        complement.s += Math.random() * 50;
+        const complementRight = complement.clone();
+        complementRight.v -= Math.random() * 40 + 10;
+        complementRight.s += Math.random() * 40 - 20;
+
+        const pivotIndex = Math.floor(Math.random() * ((this.size - 2)/2)) + Math.ceil(this.size / 2);
         this.colors = [
-                ... this.range(this.size - 1, left, right),
-                complement
-            ];
+            ... this.range(pivotIndex, left, right),
+            ... this.range(this.size - pivotIndex, complement, complementRight)
+        ];
 
         return this;
     }
